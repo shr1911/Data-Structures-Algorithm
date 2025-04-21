a0769d4cb664c2df761729e9f425379f94bd6bff
shr1911 <shraddha.mak1911@gmail.com>
2025-04-21 17:52:39 +0800

    problem solving

---
+++ src/main/java/src/problems/PartitionEqualSubsetSum.java
@@ -0,0 +1,11 @@
+//package src.problems;
+//
+//public class PartitionEqualSubsetSum {
+//    public static void main(String[] args) {
+//        System.out.println(canPartition(new int[] {1,5,11,5}));
+//    }
+//
+//    public static boolean canPartition(int[] nums) {
+//
+//    }
+//}
